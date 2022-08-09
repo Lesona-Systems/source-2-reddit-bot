6e6039367d33b857a0e96feb313fa1d6a977dcde
Lesona-Systems <nejwritings@gmail.com>
2022-08-09 18:51:10 -0500

    pep8 best practices

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import monitor as m
 
 def main():
     s2_post_exists = m.monitor()
-
     post_information = '    '.join([s2_post_exists[1], s2_post_exists[2]])
 
     if s2_post_exists[0] == True:
