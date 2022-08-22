d6eb5c0d1ac6eed0f20c533687216254bc5b56f1
Lesona-Systems <nejwritings@gmail.com>
2022-08-22 15:18:31 -0500

    implement logic that dumps out of main.py if monitor.py returns no values

--- main.py
+++ main.py
@@ -5,25 +5,28 @@ import checksum as c
 
 def main():
     new_csgo_posts = m.get_new_posts()
-    new_csgo_strings = '\/n'.join(new_csgo_posts)
-    f = open('new_csgo_posts.txt', 'w+')
-    f.write(new_csgo_strings)
-    f.close()
-
-    old_post_hash = c.hashfile('old_csgo_posts.txt')
-    new_post_hash = c.hashfile('new_csgo_posts.txt')
-
-    if c.hash_check(old_post_hash, new_post_hash) == True:
-        print('No new Source 2 posts on /r/GlobalOffensive')
-        print('Quitting...')
-        quit()
-    else:
-        f = open('old_csgo_posts.txt', 'w+')
+    if len(new_csgo_posts) > 0:
+        new_csgo_strings = '\/n'.join(new_csgo_posts)
+        f = open('new_csgo_posts.txt', 'w+')
         f.write(new_csgo_strings)
         f.close()
-        content = new_csgo_strings
-        e.email_alert(content)
-        print('Success...')
+
+        old_post_hash = c.hashfile('old_csgo_posts.txt')
+        new_post_hash = c.hashfile('new_csgo_posts.txt')
+
+        if c.hash_check(old_post_hash, new_post_hash) == True:
+            print('No NEW Source 2 posts in /r/GlobalOffensive/new')
+            print('Quitting...')
+            quit()
+        else:
+            f = open('old_csgo_posts.txt', 'w+')
+            f.write(new_csgo_strings)
+            f.close()
+            content = new_csgo_strings
+            e.email_alert(content)
+            print('Success...')
+    else:
+        print('No Source 2 posts in /r/GlobalOffensive/new')
 
 if __name__ == '__main__':
     main()
