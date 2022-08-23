74859334ab34a9fc5c742a36a1028378f95364f7
Lesona-Systems <nejwritings@gmail.com>
2022-08-23 16:42:51 -0500

    refractor main.py and combine hashcheck and hash functions in checksum.py

--- checksum.py
+++ checksum.py
@@ -1,12 +1,18 @@
 import sys
 import hashlib
 
+def checksum(file1, file2):
+    file_1_hash = hashfile(file1)
+    file_2_hash = hashfile(file2)
+
+    return hash_check(file_1_hash, file_2_hash)
+
+
 def hashfile(file):
     BUF_SIZE = 65536
     sha256 = hashlib.sha256()
 
     with open(file, 'rb') as f:
-
         while True:
             data = f.read(BUF_SIZE)
             if not data:
@@ -14,6 +20,7 @@ def hashfile(file):
 
             sha256.update(data)
         return sha256.hexdigest()
+
 
 def hash_check(hash_1, hash_2):
     if hash_1 == hash_2:
--- main.py
+++ main.py
@@ -5,29 +5,28 @@ import checksum as c
 
 def main():
     new_csgo_posts = m.get_new_posts()
+
     if new_csgo_posts:
         new_csgo_strings = '\n'.join(new_csgo_posts)
-        f = open('new_csgo_posts.txt', 'w+')
-        f.write(new_csgo_strings)
-        f.close()
+        write_file('new_csgo_posts.txt', new_csgo_strings)
 
-        old_post_hash = c.hashfile('old_csgo_posts.txt')
-        new_post_hash = c.hashfile('new_csgo_posts.txt')
-
-        if c.hash_check(old_post_hash, new_post_hash) == True:
+        if c.checksum('old_csgo_posts.txt', 'new_csgo_posts.txt'):
             print('No NEW Source 2 posts in /r/GlobalOffensive/new')
             print('Quitting...')
             quit()
         else:
-            f = open('old_csgo_posts.txt', 'w+')
-            f.write(new_csgo_strings)
-            f.close()
+            write_file('old_csgo_posts.txt', new_csgo_strings)
             content = new_csgo_strings
             e.email_alert(content)
             print('Success...')
     else:
         print('No Source 2 posts in /r/GlobalOffensive/new')
         quit()
+
+def write_file(file, content):
+    f = open(file, 'w+')
+    f.write(content)
+    f.close()
 
 if __name__ == '__main__':
     main()
